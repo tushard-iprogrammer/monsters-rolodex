cfa4e5a2ad86f3073c97ab574daa29b6307fb986
Tushar D <tushar.dahiwale@qualitiasoft.com>
2020-01-20 19:57:00 +0530

    Updates

---
+++ precache-manifest.e76b3e603d0e567b9f1c13a67aad21a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f34d6748ba4d18ca94c1bce79eaa7c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f1379950443162174104",
+    "url": "/monsters-rolodex/static/css/main.0c979bad.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1379950443162174104",
+    "url": "/monsters-rolodex/static/js/main.23d24584.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
